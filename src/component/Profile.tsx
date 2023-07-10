c18b52145a059ea6323208872bb214ec0199548f
Thayna Santana <88935936+ThaynaSantana@users.noreply.github.com>
2023-07-10 20:26:18 +0000

    estilo baseado no figma de ellen

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -9,7 +9,6 @@ interface ProfileProps {
 const Profile: React.FC<ProfileProps> = ({ currentEmotion }) => {
   const [showMenu, setShowMenu] = useState(true);
   const [buttonStyle, setButtonStyle] = useState("exit-button");
-  
 
   const toggleMenu = () => {
     setShowMenu((prevState) => !prevState);
@@ -33,23 +32,24 @@ const Profile: React.FC<ProfileProps> = ({ currentEmotion }) => {
       </div>
       {showMenu && (
         <div className="perfil">
+          <div>
+            <h3>Olá, Elis Menezes!</h3>
+            <p>Educadora Senac</p>
+            <div className="status">
+              <h2>BARRA</h2>
+              <Emotion status={currentEmotion ?? ""} />
+            </div>
+            <button className="btn btn-lg btn-info">
+              <Link className="btn-perfil" to="/meditar">
+                Respire!
+              </Link>
+            </button>
+          </div>
           <img
             id="avatar"
             src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/afro_woman_female_person-256.png"
-            //src="https://cdn-icons-png.flaticon.com/512/2920/2920072.png"
             alt="Avatar"
           />
-          <h2>Elis Menezes</h2>
-          <h3>Professora Senac</h3>
-          <div className="status">
-            <h2>Status:</h2>
-            <Emotion status={currentEmotion ?? ''} />
-          </div>
-          <button className="btn btn-lg btn-info">
-            <Link className="btn-perfil" to="/meditar">
-              Respire!
-            </Link>
-          </button>
         </div>
       )}
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
-import Profile from '../component/Profile'
-import Rodape from '../layout/Rodape'
-import Main from '../layout/Main'
-import Noticias from '../component/Noticias'
+import Profile from "../component/Profile";
+import Rodape from "../layout/Rodape";
+import Main from "../layout/Main";
+import Noticias from "../component/Noticias";
 
 const Home = () => {
   return (
     <>
       <Profile />
+      <Main />
       <Noticias />
       <Rodape />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
